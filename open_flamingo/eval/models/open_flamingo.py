99742610fc44b34d45e8f933212e290dbee5b008
haokunchen0 <haokunchen0@gmail.com>
2023-10-06 18:03:47 +0800

    更新

--- open_flamingo/eval/models/open_flamingo.py
+++ open_flamingo/eval/models/open_flamingo.py
@@ -158,7 +158,6 @@ class EvalModel(BaseEvalModel):
         batch_images: List[List[Image.Image]],
         all_class_names: List[str],
         use_cache: bool,
-        normalize_length: bool,
     ):
         """
         Returns a (B, |all_class_names|) tensor containing the logprobs for each class name.
@@ -173,81 +172,44 @@ class EvalModel(BaseEvalModel):
                 input_ids=ctx_input_ids,
                 vision_x=batch_images,
             )
-            precomputed = self.__call__(
-                vision_x=None,
-                lang_x=ctx_input_ids,
-                attention_mask=ctx_attention_mask,
-                clear_conditioned_layers=False,
-                use_cache=True,
+
+        classnames_tokens = self.tokenizer(
+                all_class_names
+            )["input_ids"]
+        # Concatenate the class name tokens
+        if not use_cache:
+            _lang_x = torch.cat([ctx_input_ids], dim=1)
+            _attention_mask = torch.cat(
+                [
+                    ctx_attention_mask,
+                ],
+                dim=1,
             )
-            precomputed_logits = precomputed.logits
-            precomputed_pkvs = precomputed.past_key_values
+            _vision_x = batch_images
         else:
-            precomputed_pkvs = None
+            _lang_x = None
+            _attention_mask = None
+            _vision_x = None
+        
+        outputs = self.__call__(
+            vision_x=_vision_x,
+            lang_x=_lang_x,
+            attention_mask=_attention_mask,
+            use_cache=use_cache
+        )
 
-        # Loop through class names and get log-likelihoods
-        # Note: if all classnames are one token, this code is redundant, since we could
-        # get all logits after one pass. However, if there are multi-token classnames,
-        # we need to loop through each classname separately.
         overall_probs = []
-        for class_name in all_class_names:
-            # Tokenize only the class name
-            classname_tokens = self.tokenizer(
-                class_name, add_special_tokens=False, return_tensors="pt"
-            )["input_ids"].to(self.device)
-            assert classname_tokens.ndim == 2
-            classname_tokens = repeat(
-                classname_tokens, "b s -> (repeat b) s", repeat=len(batch_text)
-            )
-            num_tokens_in_classname = classname_tokens.shape[1]
-
-            # Concatenate the class name tokens
-            if not use_cache:
-                _lang_x = torch.cat([ctx_input_ids, classname_tokens], dim=1)
-                _attention_mask = torch.cat(
-                    [
-                        ctx_attention_mask,
-                        torch.ones_like(classname_tokens).bool(),
-                    ],
-                    dim=1,
-                )
-                _vision_x = batch_images
-            else:
-                _lang_x = classname_tokens
-                _attention_mask = None
-                _vision_x = None
-
-            # Call forward to get the logits
-            outputs = self.__call__(
-                vision_x=_vision_x,
-                lang_x=_lang_x,
-                attention_mask=_attention_mask,
-                clear_conditioned_layers=(not use_cache),
-                past_key_values=precomputed_pkvs,
-            )
-
-            # Get the logits of the classname
-            # logits shape is either (B, num_tokens_in_classname, vocab_len) with use_cache
-            # or (B, len(_lang_x), vocab_len) without use_cache
-            # remember that the logits at index t on dim 1 correspond to predictions for the t+1st token
-            logits = outputs.logits
-            if use_cache:
-                logits = torch.cat([precomputed_logits, logits], dim=1)
-
-            logprobs = torch.log_softmax(logits, dim=-1)
-            gen_probs = logprobs[
-                :, -num_tokens_in_classname - 1 : -1, :
-            ]  # (B, num_tokens_in_classname, vocab_len)
-            gen_probs = torch.gather(
-                gen_probs, 2, classname_tokens[:, :, None]
-            ).squeeze(-1)
-
-            # Aggregate over tokens in the classname
-            if normalize_length:
-                class_prob = torch.mean(gen_probs, dim=1)
-            else:
-                class_prob = torch.sum(gen_probs, dim=1)
-            overall_probs.append(class_prob)  # (B, 1)
+        batch_size = outputs.scores[0].shape[0]
+        for classname_tokens in classnames_tokens:
+            classname_tokens_num = len(classname_tokens)
+            prob = torch.ones(batch_size).to(self.device)
+            for i in range(classname_tokens_num):
+                try:
+                    scores = torch.softmax(outputs.scores[i],dim=-1)
+                    prob *= scores[:, classname_tokens[i]]
+                except IndexError as e:
+                    prob = torch.zeros(batch_size).to(self.device)
+            overall_probs.append(prob) # (B, 1)
 
         self.uncache_media()
         overall_probs = torch.vstack(overall_probs).T.cpu()  # shape (B, num_classes)
@@ -259,7 +221,10 @@ class EvalModel(BaseEvalModel):
         vision_x: torch.Tensor,
         attention_mask: torch.Tensor,
         past_key_values: torch.Tensor = None,
-        clear_conditioned_layers: bool = False,
+        min_generation_length: int = 1,
+        max_generation_length: int = 20,
+        num_beams: int = 1,
+        length_penalty: float = 1,
         use_cache: bool = False,
     ):
         """
@@ -273,18 +238,20 @@ class EvalModel(BaseEvalModel):
         if past_key_values is None:
             with torch.inference_mode():
                 with self.autocast():
-                    outputs = self.model(
-                        vision_x=vision_x,
-                        lang_x=lang_x,
-                        attention_mask=attention_mask,
-                        clear_conditioned_layers=clear_conditioned_layers,
-                        past_key_values=past_key_values,
+                    outputs = unwrap_model(self.model).generate(
+                        vision_x,
+                        lang_x,
+                        attention_mask,
+                        min_new_tokens=min_generation_length,
+                        max_new_tokens=max_generation_length,
+                        num_beams=num_beams,
+                        length_penalty=length_penalty,
+                        output_scores=True,
+                        return_dict_in_generate=True,
                         use_cache=use_cache,
                     )
             return outputs
 
-        # loop to handle updating past_key_values
-        logits = []
         for token_idx in range(lang_x.shape[1]):
             _lang_x = lang_x[:, token_idx].reshape((-1, 1))
             if attention_mask is not None:
@@ -294,23 +261,19 @@ class EvalModel(BaseEvalModel):
 
             with torch.inference_mode():
                 with self.autocast():
-                    outputs = self.model(
-                        vision_x=vision_x,
-                        lang_x=_lang_x,
-                        attention_mask=_attention_mask,
-                        clear_conditioned_layers=False,
-                        past_key_values=past_key_values,
-                        use_cache=True,
+                    outputs = unwrap_model(self.model).generate(
+                        vision_x,
+                        _lang_x,
+                        _attention_mask,
+                        min_new_tokens=min_generation_length,
+                        max_new_tokens=max_generation_length,
+                        num_beams=num_beams,
+                        length_penalty=length_penalty,
+                        output_scores=True,
+                        return_dict_in_generate=True,
+                        use_cache=use_cache,
                     )
-
-            past_key_values = outputs.past_key_values
-            logits.append(outputs.logits)
-
-        logits = torch.cat(logits, dim=1)
-        return CausalLMOutputWithPast(
-            logits=logits,
-            past_key_values=past_key_values,
-        )
+            return outputs
 
     def encode_vision_x(self, image_tensor: torch.Tensor):
         unwrap_model(self.model)._encode_vision_x(image_tensor.to(self.device))
@@ -321,14 +284,5 @@ class EvalModel(BaseEvalModel):
     def cache_media(self, input_ids, vision_x):
         unwrap_model(self.model).cache_media(input_ids=input_ids, vision_x=vision_x)
 
-    def get_vqa_prompt(self, question, answer=None) -> str:
-        return f"<image>Question:{question} Short answer:{answer if answer is not None else ''}{'<|endofchunk|>' if answer is not None else ''}"
-
-    def get_caption_prompt(self, caption=None) -> str:
-        return f"<image>Output:{caption if caption is not None else ''}{'<|endofchunk|>' if caption is not None else ''}"
-
     def get_imagenet_prompt(self, label=None) -> str:
         return f"<image>Output:{label if label is not None else ''}{'<|endofchunk|>' if label is not None else ''}"
-
-    def get_hateful_memes_prompt(self, text, label=None) -> str:
-        return f"<image>is an image with: '{text}' written on it. Is it hateful? Answer:{label if label is not None else ''}{'<|endofchunk|>' if label is not None else ''}"
--- open_flamingo/eval/rices.py
+++ open_flamingo/eval/rices.py
@@ -3,15 +3,15 @@ import torch
 from tqdm import tqdm
 import torch
 from utils import custom_collate_fn
+import random
 
-
-class RICES:
+class RICES_Image:
     def __init__(
         self,
         dataset,
         device,
         batch_size,
-        vision_encoder_path="ViT-B-32",
+        vision_encoder_path="ViT-L-14",
         vision_encoder_pretrained="openai",
         cached_features=None,
     ):
@@ -35,7 +35,7 @@ class RICES:
 
     def _precompute_features(self):
         features = []
-
+        print(len(self.dataset))
         # Switch to evaluation mode
         self.model.eval()
 
@@ -58,6 +58,7 @@ class RICES:
                 image_features = self.model.encode_image(inputs)
                 image_features /= image_features.norm(dim=-1, keepdim=True)
                 features.append(image_features.detach())
+
 
         features = torch.cat(features)
         return features
@@ -93,3 +94,113 @@ class RICES:
 
         # Return with the most similar images last
         return [[self.dataset[i] for i in reversed(row)] for row in indices]
+
+
+class RICES_Text:
+    def __init__(
+        self,
+        dataset,
+        text_label,
+        device,
+        batch_size,
+        vision_encoder_path="ViT-L-14",
+        vision_encoder_pretrained="openai",
+        cached_features=None,
+    ):
+        self.dataset = dataset
+        self.text_label = text_label
+        self.device = device
+        self.batch_size = batch_size
+
+        # Load the model and tokenizer
+        self.model, _, self.image_processor = open_clip.create_model_and_transforms(
+            vision_encoder_path,
+            pretrained=vision_encoder_pretrained,
+        )
+        self.model = self.model.to(self.device)
+        self.tokenizer = open_clip.get_tokenizer(vision_encoder_path)
+
+        # Tokenize the text descriptions and move to device
+        self.text_tokens = self.tokenizer(text_label).to(self.device)
+
+        # Precompute features
+        if cached_features is None:
+            self.text_features = self._precompute_text_features()
+        else:
+            self.text_features = cached_features
+
+    def _precompute_text_features(self):
+        # Switch to evaluation mode
+        self.model.eval()
+
+        with torch.no_grad():
+            text_features = self.model.encode_text(self.text_tokens)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+        return text_features.detach().cpu()
+
+    def find(self, batch, num_examples):
+        """
+        Get the ranked text descriptions based on similarity to the image.
+        """
+        # Switch to evaluation mode
+        self.model.eval()
+
+        with torch.no_grad():
+            # Preprocess and encode the image
+            image_input = torch.stack([self.image_processor(image) for image in batch]).to(
+                self.device
+            )
+            #Get the feature of the input image
+            query_feature = self.model.encode_image(image_input)
+            query_feature /= query_feature.norm(dim=-1, keepdim=True)
+            query_feature = query_feature.detach().cpu()
+
+            if query_feature.ndim == 1:
+                query_feature = query_feature.unsqueeze(0)
+            # Compute the similarity between image and text representations
+            similarities = (query_feature @ self.text_features.T).squeeze()
+            if similarities.ndim == 1:
+                similarities = similarities.unsqueeze(0)
+            
+            # Get the indices of the 'num_examples' most similar texts
+            indices = similarities.argsort(dim=-1, descending=True)[:, :num_examples]
+        # Return the most similar text descriptions
+        return [[self.text_label[i] for i in row] for row in indices]
+    
+    def get_images_from_labels(self, batch_labels, shot):
+        """
+        Get 'shot' number of images for each label in the batch from the training dataset.
+        """
+        # Access dataset attributes based on dataset type
+        if hasattr(self.dataset, 'image_paths') and hasattr(self.dataset, 'labels'):
+            image_data = list(zip(self.dataset.image_paths, self.dataset.labels))
+        elif hasattr(self.dataset, 'imgs'):
+            image_data = self.dataset.imgs
+        else:
+            raise ValueError("Dataset does not have required attributes.")
+
+        print(f"Using dataset: {type(self.dataset).__name__}")
+        if hasattr(self.dataset, 'class_id_to_name'):
+            print("Dataset has class_id_to_name attribute.")
+        else:
+            print("Dataset lacks class_id_to_name attribute.")
+
+        results = []
+        for label_list in batch_labels:
+            label = label_list[0]  # As per your example, there's one label per batch item
+            matching_indices = [
+                idx for idx, (image, img_label) in enumerate(image_data)
+                if self.dataset.class_id_to_name[img_label] == label
+            ]
+            random.seed(42)
+            selected_indices = random.sample(matching_indices, shot)
+            selected_samples = [self.dataset[idx] for idx in selected_indices]
+            results.append(selected_samples)
+        return results
+
+
+
+
+
+
+
